7bbb834080936a12636e94e8c96d55e863159f00
Drenam1 <bobthetomato29@gmail.com>
2021-03-24 21:27:02 +1300

    Big game

--- scripts/lib/lib.js
+++ scripts/lib/lib.js
@@ -1,7 +1,1 @@
-async function main() {
-    let folder = game.folders.entities[0];
-    let actor = await Actor.create({ name: "New Actor", folder: folder.id });
-    console.log(actor.data.folder); // folder.id;
-    console.log(actor.folder); // folder;
-}
-main();
+console.log(game);
